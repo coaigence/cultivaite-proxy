052c30ddf31e88c90d568caf4c386dfe12e6d87a
coaigence <support@coaigence.com>
2026-05-06 22:16:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const cors    = require('cors');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const ANTHROPIC_KEY = process.env.ANTHROPIC_API_KEY;
 
